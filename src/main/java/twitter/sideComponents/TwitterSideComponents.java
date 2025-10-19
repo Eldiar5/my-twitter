568565a6fecb5d6407d4fbbd5f1aed7a768a6fe8
Eldiar <eldik0196@gmail.com>
2025-10-19 19:05:17 +0600

    Добавил эндпоинт /api/register в список разрешенных без JWT

--- src/main/java/twitter/sideComponents/TwitterSideComponents.java
+++ src/main/java/twitter/sideComponents/TwitterSideComponents.java
@@ -2,6 +2,7 @@ package twitter.sideComponents;
 
 import twitter.configuration.SideComponent;
 import twitter.configuration.SideMethod;
+import twitter.sideComponents.web.TwitterAllowedEndpoints;
 
 import java.util.Scanner;
 
@@ -11,6 +12,16 @@ public class TwitterSideComponents {
     @SideMethod
     public Scanner getScanner() {
         return new Scanner(System.in);
+    }
+
+    @SideMethod
+    public TwitterAllowedEndpoints allAllowedEndpoints() {
+        TwitterAllowedEndpoints allowedEndpoints = new TwitterAllowedEndpoints();
+
+        allowedEndpoints.addAllowedEndpoint("/api/login");
+        allowedEndpoints.addAllowedEndpoint("/api/register");
+
+        return allowedEndpoints;
     }
 
 }
