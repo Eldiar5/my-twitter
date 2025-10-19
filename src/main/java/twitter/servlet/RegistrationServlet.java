4d3e2b0b7a8786065b36e4acd313e34ef56237a3
Eldiar <eldik0196@gmail.com>
2025-10-19 19:18:10 +0600

    Создал RegistrationServlet на /api/register

---
+++ src/main/java/twitter/servlet/RegistrationServlet.java
@@ -0,0 +1,38 @@
+package twitter.servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import twitter.configuration.ComponentFactory;
+import twitter.controller.v2.RegistrationController;
+import twitter.dto.v2.request.RegistrationRequestDto;
+import twitter.dto.v2.response.RegistrationResponseDto;
+import twitter.exceptions.TwitterIllegalArgumentException;
+
+import java.io.IOException;
+
+public class RegistrationServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        RegistrationRequestDto requestDto = mapper.readValue(req.getInputStream(), RegistrationRequestDto.class);
+
+        try {
+            RegistrationController regController = ComponentFactory.getComponent(RegistrationController.class);
+            RegistrationResponseDto regResponseDto = regController.register(requestDto);
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.setContentType("application/json");
+
+            resp.getWriter().write(mapper.writeValueAsString(regResponseDto));
+        } catch (TwitterIllegalArgumentException ex) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().write(mapper.writeValueAsString(ex.getMessage()));
+        } catch (Exception e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().write(mapper.writeValueAsString(e.getMessage()));
+        }
+    }
+}
