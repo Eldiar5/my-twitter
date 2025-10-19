d82489edfaee8e586edacce93f247b93d81a4d39
Eldiar <eldik0196@gmail.com>
2025-10-19 19:20:12 +0600

    Создал класс TwitterEndpointFilter для фильтрации эндпоинтов

---
+++ src/main/java/twitter/filter/TwitterEndpointFilter.java
@@ -0,0 +1,40 @@
+package twitter.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import twitter.configuration.ComponentFactory;
+import twitter.security.JwtHandler;
+import twitter.sideComponents.web.TwitterAllowedEndpoints;
+
+import java.io.IOException;
+
+public class TwitterEndpointFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        TwitterAllowedEndpoints allowedEndpoints = ComponentFactory.getComponent(TwitterAllowedEndpoints.class);
+        if (allowedEndpoints.isEndpointAllowed(request.getRequestURI())) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        HttpServletResponse  httpServletResponse = (HttpServletResponse) servletResponse;
+        String authorization = request.getHeader("Authorization");
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        String token = authorization.substring(7);
+        JwtHandler jwtHandler = ComponentFactory.getComponent(JwtHandler.class);
+        if (!jwtHandler.validateToken(token)) {
+            httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
