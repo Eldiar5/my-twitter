d65dc27f940c4c997839416fb64452ec3d3148d5
Eldiar <eldik0196@gmail.com>
2025-10-20 16:58:33 +0600

    Заменил маппинг сущности внутри контроллера, на вызов метода mapUserToResponseDto()

--- src/main/java/twitter/controller/v2/InfoController.java
+++ src/main/java/twitter/controller/v2/InfoController.java
@@ -3,29 +3,27 @@ package twitter.controller.v2;
 import twitter.configuration.Component;
 import twitter.configuration.Injection;
 import twitter.dto.v2.response.InfoResponseDto;
-import twitter.entity.user.Organization;
-import twitter.entity.user.Person;
 import twitter.entity.user.User;
-import twitter.entity.user.UserType;
 import twitter.exceptions.TwitterIllegalArgumentException;
 import twitter.exceptions.UserNotFoundException;
+import twitter.mapper.v2.HttpUserMapper;
 import twitter.service.UserService;
 
-import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 @Component
 public class InfoController {
 
     private final UserService userService;
+    private final HttpUserMapper userMapper;
 
     @Injection
-    public InfoController(UserService userService) {
+    public InfoController(UserService userService, HttpUserMapper userMapper) {
         this.userService = userService;
+        this.userMapper = userMapper;
     }
 
     public InfoResponseDto info(String login) {
-
         if (Objects.isNull(login) ||  login.isBlank()) {
             throw new TwitterIllegalArgumentException("Некорректное имя пользователя");
         }
@@ -37,30 +35,10 @@ public class InfoController {
                 throw new UserNotFoundException("Не удалось найти пользователя с логином: " + login);
             }
 
-            InfoResponseDto responseDto = new InfoResponseDto();
-            responseDto.setId(user.getId());
-            responseDto.setLogin(user.getLogin());
-
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy");
-            if (UserType.PERSON.equals(user.getUserType())) {
-                Person person = (Person) user;
-                responseDto.setName(person.getName());
-                responseDto.setLastName(person.getLastName());
-                responseDto.setBirthDate(person.getBirthDate().format(formatter));
-            }
-
-            if (UserType.ORGANIZATION.equals(user.getUserType())) {
-                Organization organization = (Organization) user;
-                responseDto.setTitle(organization.getTitle());
-                responseDto.setSpecialization(organization.getSpecialization());
-                responseDto.setDateOfFoundation(organization.getDateOfFoundation().format(formatter));
-            }
-
-            return responseDto;
+            return userMapper.mapUserToResponseDto(user);
         } catch (UserNotFoundException ex) {
             throw new TwitterIllegalArgumentException(ex.getMessage());
         }
-
     }
 
 }
