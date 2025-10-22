a17c71a5241968590ca26e2a9d68c771525a77ff
Eldiar <eldik0196@gmail.com>
2025-10-22 12:06:57 +0600

    Реализовал обработку команды addPost на эндпоинт /api/add-post

--- src/main/java/twitter/controller/v2/PostController.java
+++ src/main/java/twitter/controller/v2/PostController.java
@@ -1,4 +1,75 @@
 package twitter.controller.v2;
 
+import twitter.configuration.Component;
+import twitter.configuration.Injection;
+import twitter.dto.v2.request.PostRequestDto;
+import twitter.dto.v2.response.PostResponseDto;
+import twitter.entity.post.Post;
+import twitter.entity.user.User;
+import twitter.exceptions.TwitterIllegalArgumentException;
+import twitter.exceptions.UserNotFoundException;
+import twitter.service.PostService;
+import twitter.service.UserService;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Objects;
+
+@Component
 public class PostController {
+
+    private final PostService postService;
+    private final UserService userService;
+
+    @Injection
+    public PostController(PostService postService, UserService userService) {
+        this.postService = postService;
+        this.userService = userService;
+    }
+
+    public PostResponseDto addPost(PostRequestDto postRequestDto, String author) {
+        this.validateField(postRequestDto.getTopic(), "Тема публикации не может быть пустой.");
+
+        this.validateField(postRequestDto.getText(), "Текст публикации не может быть пустой.");
+
+        try {
+            User user = userService.getUserByLogin(author);
+
+            Post post = new Post();
+
+            post.setTopic(postRequestDto.getTopic());
+            post.setText(postRequestDto.getText());
+
+            if (!Objects.isNull(postRequestDto)) {
+                post.setTags(postRequestDto.getTags().toArray(new String[0]));
+            } else {
+                post.setTags(new String[0]);
+            }
+
+            post.setAuthorId(user.getId().intValue());
+            post.setCreatedAt(LocalDateTime.now());
+
+            Post savedPost = postService.savePost(post);
+
+            PostResponseDto postResponseDto = new PostResponseDto();
+            postResponseDto.setAuthorLogin(user.getLogin());
+            postResponseDto.setPostId(savedPost.getPostId());
+            postResponseDto.setTopic(savedPost.getTopic());
+            postResponseDto.setText(savedPost.getText());
+            postResponseDto.setTags(Arrays.asList(savedPost.getTags()));
+            postResponseDto.setCreatedAt(savedPost.getCreatedAt());
+
+            return postResponseDto;
+        } catch (UserNotFoundException ex) {
+            throw new TwitterIllegalArgumentException(ex.getMessage());
+        } catch (Exception ex) {
+            throw new TwitterIllegalArgumentException("Не удалось создать пост: " + ex.getMessage());
+        }
+    }
+
+    private void validateField(String field, String errorMessage) {
+        if (Objects.isNull(field) || field.trim().isEmpty()) {
+            throw new TwitterIllegalArgumentException(errorMessage);
+        }
+    }
 }
---
+++ src/main/java/twitter/servlet/AddPostCommandServlet.java
@@ -0,0 +1,48 @@
+package twitter.servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import twitter.configuration.ComponentFactory;
+import twitter.controller.v2.PostController;
+import twitter.dto.v2.request.PostRequestDto;
+import twitter.dto.v2.response.PostResponseDto;
+import twitter.exceptions.TwitterIllegalArgumentException;
+import twitter.security.JwtHandler;
+import twitter.sideComponents.web.ObjectMapperAsComponent;
+
+import java.io.IOException;
+
+public class AddPostCommandServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper mapper = ComponentFactory.getComponent(ObjectMapperAsComponent.class).getObjectMapper();
+        JwtHandler jwtHandler = ComponentFactory.getComponent(JwtHandler.class);
+
+        String authorization = req.getHeader("Authorization");
+        String token = authorization.substring(7);
+
+        PostRequestDto postRequestDto = mapper.readValue(req.getReader(), PostRequestDto.class);
+
+        try {
+            String user = jwtHandler.getUsernameFromToken(token);
+            PostController  postController = ComponentFactory.getComponent(PostController.class);
+            PostResponseDto responseDto = postController.addPost(postRequestDto, user);
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.setContentType("application/json");
+
+            resp.getWriter().write(mapper.writeValueAsString(responseDto));
+
+        } catch (TwitterIllegalArgumentException ex) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().write(mapper.writeValueAsString(ex.getMessage()));
+        } catch (Exception e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().write(mapper.writeValueAsString(e.getMessage()));
+        }
+    }
+}
